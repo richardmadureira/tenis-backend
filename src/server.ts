8d2502f37a26bafc4edef22e6e03770a8be57c42
Richard Mendes Madureira <richard.madureira@dataprev.gov.br>
2022-11-25 07:36:50 -0300

    adicionado helmet e cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+app.disable('x-powered-by');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
 app.use(routes);
+
 
 app.listen(3333);
